38043b40cc43c8af381eabdc46e42496dd091d6b
HappyJeannie <436030684@qq.com>
2018-05-23 23:37:12 +0800

    修改留言 JS

--- js/message.js
+++ js/message.js
@@ -6,6 +6,24 @@
     appId: APP_ID,
     appKey: APP_KEY
   });
+  let imgs = ['0.png','1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png'];
+  let myForm = document.querySelector('#postMessageForm');
+  myForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    let name = myForm.querySelector('input[name="name"]').value;
+    let content = myForm.querySelector('input[name="content"]').value;
+    //创建表
+    let Message = AV.Object.extend('Message');
+    //创建表中的一条数据
+    let message = new Message();
+    //存储一条数据到表中
+    message.save({
+      'name': name,
+      'msg':content
+    }).then(function(object) {//保存成功的回调
+      console.log(object);
+    })
+  })
   // //创建表
   // var Message = AV.Object.extend('Message');
   // //创建表中的一条数据

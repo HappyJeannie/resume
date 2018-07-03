5f3cd2b99ddce65e335ef874f8d9485991b1654f
HappyJeannie <436030684@qq.com>
2018-07-03 23:30:30 +0800

    打包文件

---
+++ src/index/js/index.js
@@ -0,0 +1,11 @@
+import './../../../assets/css/base.scss';
+import './../../../assets/css/load.scss';
+import './../../../vendor/swiper/swiper.min.css';
+import './../css/main.scss';
+window.onload = function(){
+  let a = 'asdfghjkl';
+  let b = 2;
+  let c = a + b;
+  alert(1);
+  console.log(c);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,69 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index/js/index.js',
+  entry: {
+    index:'./src/index/js/index.js'
+  },
+  devtool:'inline-source-map',
+  devServer:{
+    contentBase : './dist'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'my-first-webpack.bundle.js'
+    filename: 'app.bundle.js'
+  },
+  plugins:[
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename:'index.html',
+      template:'./src/index/index.html',
+      chunks:['index'] 
+    })
+  ],
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, 
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader" 
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use:[
+          {
+            loader:'file-loader',
+            options:{
+              name: '[name][hash].[ext]',
+              outputPath: 'images/'
+            }
+          }
+          
+        ]
+      },
+      {
+        test: /\.(html|htm|)$/,
+        use: [
+          'html-withimg-loader'
+        ]
+      }
+    ]
   }
 };
